24bf0ae250d6667c1b2deb91818c4f77b9d4be55
zach-withcoherence <86851918+zach-withcoherence@users.noreply.github.com>
2024-07-18 19:02:32 -0400

    migrations and FE fixes

--- frontend/build.sh
+++ frontend/build.sh
@@ -3,6 +3,7 @@
 # Read the API_URL from an environment variable
 API_URL=${CNC_ENVIRONMENT_DOMAIN:-http://localhost}
 
-# Use sed to replace the template variable in the HTML file
-sed "s|\${API_URL}|$API_URL|g" index.html.tpl > assets/index.html
-sed "s|\${ENVIRONMENT_NAME}|$CNC_ENVIRONMENT_NAME|g" assets/index.html > assets/index.html
+# Use sed to replace both template variables in the HTML file
+sed -e "s|\${API_URL}|$API_URL|g" \
+    -e "s|\${ENVIRONMENT_NAME}|$CNC_ENVIRONMENT_NAME|g" \
+    index.html.tpl > assets/index.html
