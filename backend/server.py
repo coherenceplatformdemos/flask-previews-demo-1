3218e637b4f817721076c5d94c6b56a7d8e3b0e8
zach-withcoherence <86851918+zach-withcoherence@users.noreply.github.com>
2024-07-18 16:43:57 -0400

    improvements

--- backend/server.py
+++ backend/server.py
@@ -27,12 +27,12 @@ class Todo(db.Model):
 with app.app_context():
     db.create_all()
 
-@app.route('/todos', methods=['GET'])
+@app.route('/api/todos', methods=['GET'])
 def get_todos():
     todos = Todo.query.all()
     return jsonify([todo.to_dict() for todo in todos])
 
-@app.route('/todos', methods=['POST'])
+@app.route('/api/todos', methods=['POST'])
 def create_todo():
     data = request.json
     new_todo = Todo(title=data['title'])
@@ -40,7 +40,7 @@ def create_todo():
     db.session.commit()
     return jsonify(new_todo.to_dict()), 201
 
-@app.route('/todos/<int:todo_id>', methods=['PUT'])
+@app.route('/api/todos/<int:todo_id>', methods=['PUT'])
 def update_todo(todo_id):
     todo = Todo.query.get_or_404(todo_id)
     data = request.json
@@ -49,7 +49,7 @@ def update_todo(todo_id):
     db.session.commit()
     return jsonify(todo.to_dict())
 
-@app.route('/todos/<int:todo_id>', methods=['DELETE'])
+@app.route('/api/todos/<int:todo_id>', methods=['DELETE'])
 def delete_todo(todo_id):
     todo = Todo.query.get_or_404(todo_id)
     db.session.delete(todo)
@@ -57,4 +57,5 @@ def delete_todo(todo_id):
     return '', 204
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
